6285757179d5f27df7ed8b9bb41ea3ae3d3db691
Aj-github-cmd <sahooamlanjyoti99@gmail.com>
2021-12-08 22:58:50 +0530

    mark 9 project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
-
+import travelDb from './api/TravelDb';
+import {AiFillGithub,AiOutlineTwitter} from 'react-icons/ai'
 function App() {
+  const [selectedseason, setSelectedseason] = useState("Summer")
+  const selectHandler =(season)=>{
+    setSelectedseason(season)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="heading"><h3>Welcome to Travel Recommendations app</h3></div>
+    <div className="seasons"> 
+    {
+       Object.keys(travelDb).map((season)=>{
+       return(
+         <><button onClick={()=>{selectHandler(season)}}
+         >{season}</button></>
+       )
+      })
+    }
+    </div>
+    <div className="travelinfo">
+    <div className="places">
+     {
+       travelDb[selectedseason].map((elem)=>{
+        return(<>
+        <div className="place">
+        <h4>Months : {elem.duration}</h4>
+        <h3>Place to visit : {elem.place}</h3>
+        </div>
+        </>)
+       }) 
+     } 
+    </div>
+    </div>
+    <div className="footer">
+      <h3>Github repo <AiFillGithub 
+      style={{color:"#000000",fontSize:"35px",cursor:"pointer"}}/></h3>
+      <h3>Connect with me <AiOutlineTwitter
+       style={{color:"#000000",fontSize:"35px",cursor:"pointer"}}
+      /></h3>
+    </div>
     </div>
   );
 }
---
+++ src/api/TravelDb.js
@@ -0,0 +1,19 @@
+const travelDb = {
+    Summer:[
+    { "duration":"May-June", "place":"Udaipur"},
+    { "duration":"June-July", "place":"Darjeeling"},
+    { "duration":"May-July", "place":"Kashmir"}
+    ],
+    Winter:[
+    { "duration":"November-December", "place":"Gulmarg"},
+    { "duration":"January-Februaray", "place":"Shimla-Kufri"},
+    { "duration":"December-February", "place":"Dalhousie"},
+    ],
+    Monsoon:[
+    { "duration":"August-September", "place":"Shillong"},
+    { "duration":"September-October", "place":"Munnar"},
+    { "duration":"August-September", "place":"Lonavala"},
+    { "duration":"August-October", "place":"Kutch"}
+    ]
+}
+module.exports = travelDb;
